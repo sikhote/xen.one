b1fc6e35b600f0bcd19e1c4f6a85d49555cfb5f4
David Sinclair <dsincla@rei.com>
2024-07-13 19:18:36 -0700

    updates

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
+import { Analytics } from '@vercel/analytics/react';
 import 'lib/styles.scss';
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
